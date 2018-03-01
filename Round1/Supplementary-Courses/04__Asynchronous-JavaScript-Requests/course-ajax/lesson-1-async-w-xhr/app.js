2bebce64538fd7b1e3ab74447af8d98727d0462e
Kevin Urban <kevin.ddu@gmail.com>
2018-03-01 08:24:43 -0500

    Add Unsplash API image function

--- Round1/Supplementary-Courses/04__Asynchronous-JavaScript-Requests/course-ajax/lesson-1-async-w-xhr/app.js
+++ Round1/Supplementary-Courses/04__Asynchronous-JavaScript-Requests/course-ajax/lesson-1-async-w-xhr/app.js
@@ -8,5 +8,34 @@
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.onload = addImage;
+        unsplashRequest.onerror = function(err) {
+          requestError(err, 'image');
+        };
+        unsplashRequest.open('GET', 
+          `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.setRequestHeader('Authorization', `Client-ID ${unsplashId}`);
+        unsplashRequest.send();
+
     });
+
+  // Unsplash Images
+  function addImage() {
+    //debugger;//initially used to monitor response in DevTools
+    let htmlContent = '';
+    const data = JSON.parse(this.responseText);
+    const firstImage = data.results[0];
+    if(data && data.results && data.results[0]) {
+      htmlContent = `<figure>
+        <img src="${firstImage.urls.regular}" alt=${searchedForText}">
+        <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+      <figure>`;
+    } else {
+      htmlContent = '<div class="error-no-image">No images available</div>'
+    }
+    responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+  };
+
 })();
